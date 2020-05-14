a50efcf54ad4168245bc8e0343959b1dc85ad606
floriansr <sueurfloriansf@gmail.com>
2020-05-14 23:26:09 +0200

    Updates

---
+++ precache-manifest.73697f7e84253f8e6ac40c7a6a26631e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8469070e76f63608de8bd88dffc420e",
+    "url": "/International_Agency/index.html"
+  },
+  {
+    "revision": "3cb86a08ab9534b8c979",
+    "url": "/International_Agency/static/css/2.35fcc229.chunk.css"
+  },
+  {
+    "revision": "a220e3083633533ea86e",
+    "url": "/International_Agency/static/css/main.13a5c3c9.chunk.css"
+  },
+  {
+    "revision": "3cb86a08ab9534b8c979",
+    "url": "/International_Agency/static/js/2.2cec0fd4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/International_Agency/static/js/2.2cec0fd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a220e3083633533ea86e",
+    "url": "/International_Agency/static/js/main.20c31962.chunk.js"
+  },
+  {
+    "revision": "de2d05e52a95a3144acc",
+    "url": "/International_Agency/static/js/runtime-main.d59500aa.js"
+  }
+]);
